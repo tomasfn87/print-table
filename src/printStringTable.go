7c5adf20d00793730811c41d0136cc253b2e80e0
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-02-17 18:53:48 -0300

    Changed file 'src/printStringTable.go': 1): added 2 functions: 1.1): 'fillWithTimes': receives a string into 'char', ideally only one character, and an integer into 'size', that corresponds to the length of the desired string. The function uses the first character of 'char' as the base value, and returns a string whose content is 'size' times 'char[0]' (the first character in 'char'). 1.2): 'getFirstNotEmptyChar': receives a string and returns its first character that is different from an empty space (" ") or an empty space, if there are no other characters. 2): changed function 'PrintStrArr': it now receives a string instead of a rune. 3): changed function 'PrintStrTable': the 'gap' parameter has been divided into two parameters: 3.1): 'gapChar' e 3.2): 'gapLength'. 3.1) receives a string like 'gap' did, but it will be filtered through 'getFirstNotEmptyChar', that will check if there is a character different from an empty space in 'gapChar', returning the first one it encounters, or an empty space (" ") if the end of the string is reached. This caracter will be used to separate columns if 'gapLength' receives a value equal or higher than 2; if the value is equal or lower than 1, 'gapLength' will assume value 1. Each column will be separated by a string with the length of 'gapLength'. If 'gapLength' is 1, it will discard 'gapChar' an use an empty space; if it's 2, each column will be separated by a string composed of 2 characters: 'gapChar' and an empty space; if 'gapLength' is 3, the columns will be separated by a 3 character string composed of one empty space, 'gapChar' and another empty space. Finally, if 'gapChar' is equal to an empty space (" "), the columns will simply be separated by 1, 2 or 3 empty spaces. 3.3): added a header to the table impression, composed of hifens ("-") and pluses ("+").

--- src/printStringTable.go
+++ src/printStringTable.go
@@ -1,6 +1,8 @@
 package table
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type StrArr struct {
 	Arr []string
@@ -56,20 +58,55 @@ func alignLeft(text string, size int) string {
 	return text + suffix
 }
 
-func (a StrArr) PrintStrArr(align rune) {
+func fillWithTimes(char string, size int) string {
+	fill := ""
+	for i := 0; i < size; i++ {
+		fill += fmt.Sprintf("%c", char[0])
+	}
+	return fill
+}
+
+func getFirstNotEmptyChar(text string) string {
+	for _, v := range text {
+		if fmt.Sprintf("%c", v) != " " {
+			return fmt.Sprintf("%c", v)
+		}
+	}
+	return " "
+}
+
+func (a StrArr) PrintStrArr(align string) {
 	margin := a.getLongestString()
 	lengths := a.getEachItemLength()
 	for k, v := range a.Arr {
 		switch align {
-		case 'R':
+		case "R":
 			fmt.Printf("%s\n", alignRight(v, margin-lengths[k]))
-		case 'L':
+		default:
 			fmt.Printf("%s\n", alignLeft(v, margin-lengths[k]))
 		}
 	}
 }
 
-func (t StrTable) PrintStrTable(align rune, gap string) {
+func (t StrTable) PrintStrTable(align string, gapChar string, gapLength int) {
+	gapChar = getFirstNotEmptyChar(gapChar)
+
+	if gapLength < 1 {
+		gapLength = 1
+	} else if gapLength > 3 {
+		gapLength = 3
+	}
+
+	gap := ""
+	switch gapLength {
+	case 1:
+		gap = " "
+	case 2:
+		gap = gapChar + " "
+	default:
+		gap = " " + gapChar + " "
+	}
+
 	marginArr := make([]int, len(t.ArrArr))
 	for k := range marginArr {
 		marginArr[k] = t.ArrArr[k].getLongestString()
@@ -82,13 +119,30 @@ func (t StrTable) PrintStrTable(align rune, gap string) {
 
 	numItems := t.ArrArr[:][0].getArrayLength()
 
+	firstLine := ""
+	betweenColumns := ""
+	switch len(gap) {
+	case 1:
+		betweenColumns += "+"
+	case 2:
+		betweenColumns += "+-"
+	case 3:
+		betweenColumns += "-+-"
+	}
+	for k := range marginArr {
+		firstLine += fmt.Sprint(fillWithTimes("-", marginArr[k]))
+		if k != len(t.ArrArr)-1 {
+			firstLine += betweenColumns
+		}
+	}
+	fmt.Println(firstLine)
+
 	for i := 0; i < numItems; i++ {
 		line := ""
 		for k, v := range t.ArrArr {
-			switch align {
-			case 'R':
+			if align == "R" {
 				line += alignRight(v.Arr[i], marginArr[k]-lengthsArr[k][i])
-			case 'L':
+			} else {
 				line += alignLeft(v.Arr[i], marginArr[k]-lengthsArr[k][i])
 			}
 			if k != len(t.ArrArr)-1 {
