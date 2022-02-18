e7b43a6f1d5036022d0139e710ffe54ebe20b212
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-02-18 13:45:15 -0300

    Changed file 'src/printStringTable.go': 1): added 3 structs to organize 'PrintStrTable's parameters and to guide its declaration: 1.1): Interval{ Start int, End int }; 1.2): Gap{ Char string, Length int }; 1.3): Align{ Position string }. 1.1) receives the starting and the ending element to be printed; 1.2) receives the character that will be used between each column and how characters long it will be (1, 2 or 3); 1.3): receives the alignment position: "R" to align table elements to the right; "L" or any other string to align table elements to the left (it accepts any string because it's the default alignment). 2): changed functions: 2.1): 'getLongestString'; 2.2): 'getEachItemLength': both functions now have a single parameter 'i', of type 'Interval': it's used to correctly calculate the margins when printing smaller slices of a table. 3): changed function 'PrintStrArr': the function now receives two parameters (instead of one): 'i' of type 'Interval' and 'al' of type 'Align': 'i.Start' and 'i.End' are used to define the starting and the ending point of the array's slice to be printed and to correctly align them before doing so; 'al.Position' tells the function how to align the values (default 'L' left  or 'R' right). 4): added function 'getGapBetweenColumns': the function has a parameter 'g' of type 'Gap', whose values 'Char' and 'Length' are used to define the string used to as a separator between each column. 5): changed functions: 5.1): 'getMargins'; 5.2): 'getLengths': both functions now have a single parameter 'i', of type 'Interval': it's used to correctly align each array's elements to the left or right, taking the slice of the table into account. 6): added two functions: 6.1): 'printTableFirstLine'; 6.2): 'printTableBody': these functions' contents were part of 'PrintStrTable', and were extracted to improve the code's organization, modularity and readability. 6.1) has two parameters 'i', of type 'Interval', and 'gap', of the type string, and prints the first line of a table, formed by '-' and '+', the latter is used to mark each column's beginnin. 6.2): receives three paramters: 'i' of type 'Interval', 'al' of type 'Align' and 'gap' of type string: the function assembles and prints each line of the table's body. 7): changed function 'PrintStrTable': the function is quite smaller, and still has three parameters, but different ones: 'align' is now 'al'; 'gapChar' and 'gapLength' are now into 'gap': 'gapChar' -> 'gap.Char'; 'gapLength' -> 'gap.Length'. Its first parameter is 'i', of type 'Interval', and determines if the whole table or just a smaller slice of it will be printed, according to 'i.Start' (0 to start at the first value) and 'i.End' (0 to print the whole table, or a value smaller than the table's length to skip the last values.

--- src/printStringTable.go
+++ src/printStringTable.go
@@ -12,34 +12,26 @@ type StrTable struct {
 	ArrArr []StrArr
 }
 
+type Interval struct {
+	Start int
+	End   int
+}
+
+type Gap struct {
+	Char   string
+	Length int
+}
+
+type Align struct {
+	Position string
+}
+
 func NewStrArr(a StrArr) *StrArr {
 	return &StrArr{Arr: a.Arr}
 }
 
 func NewStrTable(t StrTable) *StrTable {
 	return &StrTable{ArrArr: t.ArrArr}
-}
-
-func (a StrArr) getLongestString() int {
-	longestStrLength := 0
-	for _, v := range a.Arr {
-		if len(v) > longestStrLength {
-			longestStrLength = len(v)
-		}
-	}
-	return longestStrLength
-}
-
-func (a StrArr) getEachItemLength() []int {
-	lengths := make([]int, len(a.Arr), len(a.Arr))
-	for k, v := range a.Arr {
-		lengths[k] = len(v)
-	}
-	return lengths
-}
-
-func (a StrArr) getArrayLength() int {
-	return len(a.Arr)
 }
 
 func alignRight(text string, size int) string {
@@ -75,11 +67,33 @@ func getFirstNotEmptyChar(text string) string {
 	return " "
 }
 
-func (a StrArr) PrintStrArr(align string) {
-	margin := a.getLongestString()
-	lengths := a.getEachItemLength()
+func (a StrArr) getLongestString(i Interval) int {
+	longestStrLength := 0
+	for _, v := range a.Arr[i.Start:i.End] {
+		if len(v) > longestStrLength {
+			longestStrLength = len(v)
+		}
+	}
+	return longestStrLength
+}
+
+func (a StrArr) getEachItemLength(i Interval) []int {
+	lengths := make([]int, len(a.Arr[i.Start:i.End]))
+	for k, v := range a.Arr[i.Start:i.End] {
+		lengths[k] = len(v)
+	}
+	return lengths
+}
+
+func (a StrArr) getArrayLength(i Interval) int {
+	return len(a.Arr[i.Start:i.End])
+}
+
+func (a StrArr) PrintStrArr(i Interval, al Align) {
+	margin := a.getLongestString(i)
+	lengths := a.getEachItemLength(i)
 	for k, v := range a.Arr {
-		switch align {
+		switch al.Position {
 		case "R":
 			fmt.Printf("%s\n", alignRight(v, margin-lengths[k]))
 		default:
@@ -88,37 +102,43 @@ func (a StrArr) PrintStrArr(align string) {
 	}
 }
 
-func (t StrTable) PrintStrTable(align string, gapChar string, gapLength int) {
-	gapChar = getFirstNotEmptyChar(gapChar)
-
-	if gapLength < 1 {
-		gapLength = 1
-	} else if gapLength > 3 {
-		gapLength = 3
+func (t StrTable) getGapBetweenColumns(g Gap) string {
+	g.Char = getFirstNotEmptyChar(g.Char)
+	if g.Length < 1 {
+		g.Length = 1
+	} else if g.Length > 3 {
+		g.Length = 3
 	}
 
 	gap := ""
-	switch gapLength {
+	switch g.Length {
 	case 1:
 		gap = " "
 	case 2:
-		gap = gapChar + " "
+		gap = g.Char + " "
 	default:
-		gap = " " + gapChar + " "
+		gap = " " + g.Char + " "
 	}
+	return gap
+}
 
+func (t StrTable) getMargins(i Interval) []int {
 	marginArr := make([]int, len(t.ArrArr))
-	for k := range marginArr {
-		marginArr[k] = t.ArrArr[k].getLongestString()
+	for k := range marginArr[:] {
+		marginArr[k] = t.ArrArr[k].getLongestString(i)
 	}
+	return marginArr
+}
 
+func (t StrTable) getLengths(i Interval) [][]int {
 	lengthsArr := make([][]int, len(t.ArrArr))
-	for k := range lengthsArr {
-		lengthsArr[k] = t.ArrArr[k].getEachItemLength()
+	for k := range lengthsArr[:] {
+		lengthsArr[k] = t.ArrArr[k].getEachItemLength(i)
 	}
+	return lengthsArr
+}
 
-	numItems := t.ArrArr[:][0].getArrayLength()
-
+func (t StrTable) printTableFirstLine(i Interval, gap string) {
 	firstLine := ""
 	betweenColumns := ""
 	switch len(gap) {
@@ -129,6 +149,7 @@ func (t StrTable) PrintStrTable(align string, gapChar string, gapLength int) {
 	case 3:
 		betweenColumns += "-+-"
 	}
+	marginArr := t.getMargins(i)
 	for k := range marginArr {
 		firstLine += fmt.Sprint(fillWithTimes("-", marginArr[k]))
 		if k != len(t.ArrArr)-1 {
@@ -136,14 +157,21 @@ func (t StrTable) PrintStrTable(align string, gapChar string, gapLength int) {
 		}
 	}
 	fmt.Println(firstLine)
+}
 
-	for i := 0; i < numItems; i++ {
+func (t StrTable) printTableBody(i Interval, al Align, gap string) {
+	numItems := t.ArrArr[:][0].getArrayLength(i)
+	marginArr := t.getMargins(i)
+	lengthsArr := t.getLengths(i)
+
+	for j := 0; j < numItems; j++ {
 		line := ""
-		for k, v := range t.ArrArr {
-			if align == "R" {
-				line += alignRight(v.Arr[i], marginArr[k]-lengthsArr[k][i])
+		for k, v := range t.ArrArr[:] {
+			v.Arr = v.Arr[i.Start:i.End]
+			if al.Position == "R" {
+				line += alignRight(v.Arr[j], marginArr[k]-lengthsArr[k][j])
 			} else {
-				line += alignLeft(v.Arr[i], marginArr[k]-lengthsArr[k][i])
+				line += alignLeft(v.Arr[j], marginArr[k]-lengthsArr[k][j])
 			}
 			if k != len(t.ArrArr)-1 {
 				line += gap
@@ -151,4 +179,13 @@ func (t StrTable) PrintStrTable(align string, gapChar string, gapLength int) {
 		}
 		fmt.Println(line)
 	}
+}
+
+func (t StrTable) PrintStrTable(i Interval, al Align, g Gap) {
+	if i.End == 0 {
+		i.End = len(t.ArrArr[:][0].Arr)
+	}
+	gap := t.getGapBetweenColumns(g)
+	t.printTableFirstLine(i, gap)
+	t.printTableBody(i, al, gap)
 }
